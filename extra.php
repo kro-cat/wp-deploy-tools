f9a05df1c7d877a5c259dbd0123877f9ef492672
Kara <kodell@kbisp.com>
2023-08-10 13:19:50 -0500

    update

--- extra.php
+++ extra.php
@@ -4,3 +4,19 @@ define( 'WP_DEBUG_DISPLAY', false );
 @ini_set( 'display_errors', 0 );
 @error_reporting( E_ALL );
 
+define('FS_METHOD', 'direct');
+
+define('WP_ENVIRONMENT_TYPE', 'development');
+
+try {
+if (defined('WP_ENVIRONMENT_TYPE') && (constant('WP_ENVIRONMENT_TYPE') !== 'production')) {
+	if (defined('WP_CLI')) {
+		$_SERVER['HTTP_HOST'] = '127.0.0.1:80';
+	}
+	if (!isset($_SERVER['HTTP_REQUEST_SCHEME'])) {
+		$_SERVER['HTTP_REQUEST_SCHEME'] = 'http';
+	}
+	define( 'WP_HOME', $_SERVER['HTTP_REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' );
+	define( 'WP_SITEURL', $_SERVER['HTTP_REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' );
+}
+} catch (\Throwable $e) {}
